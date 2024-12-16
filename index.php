8b6e035399a03540c02a400f8a4c29bc2212b9b7
VincentTan8 <vincent.tan@wetalk.com>
2024-12-16 14:42:04 +0800

    feat: added fields for card and customer creation

--- index.php
+++ index.php
@@ -26,8 +26,29 @@
             <button id="payBtn">Pay Now</button>
         </div>
         <div class="window">
-            <span class="text">Pay and Add Card</span>
+            <span class="text">Add Customer</span>
+            <label for="phoneField">Enter phone number below:</label>
+            <input type="text" class="phoneField" name="phoneField" placeholder="+639991234567">
+            <label for="emailField">Enter email below:</label>
+            <input type="text" class="emailField" name="emailField" placeholder="maya.juan@gmail.com">
+            <label for="fNameField">Enter first name below:</label>
+            <input type="text" class="fNameField" name="fNameField" placeholder="Pay">
+            <label for="mNameField">Enter middle name below:</label>
+            <input type="text" class="mNameField" name="mNameField" placeholder="Maya">
+            <label for="lNameField">Enter last name below:</label>
+            <input type="text" class="lNameField" name="lNameField" placeholder="Juan">
             <button id="addCustomerBtn">Add customer</button>
+        </div>
+        <div class="window">
+            <span class="text">Add Card</span>
+            <label for="cardNumField">Enter card num below:</label>
+            <input type="text" class="cardNumField" name="cardNumField" placeholder="Card number here">
+            <label for="expMonthField">Enter expiry month below:</label>
+            <input type="text" class="expMonthField" name="expMonthField" placeholder="Expiry month here">
+            <label for="expYearField">Enter expiry year below:</label>
+            <input type="text" class="expYearField" name="expYearField" placeholder="Expiry year here">
+            <label for="cvcField">Enter cvc below:</label>
+            <input type="text" class="cvcField" name="cvcField" placeholder="CVC here">
             <button id="addCardBtn">Add card</button>
             <button id="payAndSaveBtn">Pay and Save a Card</button>
         </div>
